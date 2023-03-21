dbc6edf9aabbb8545c2ec447775c8fa35055d39f
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-21 16:45:06 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominic M.
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const houseNumber = document.getElementById("house-number").value
+  const streetName = document.getElementById("street-name").value
+  document.getElementById("address").innerHTML = "Your address is: " + houseNumber + " " + streetName
+}
