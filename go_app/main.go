86b6e4285dba75978892feed3407edf5f69071fe
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-21 16:53:25 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,27 @@
+// Created by: Dominic M.
+// Created on: March 2023
+//
+// This program does addition
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function does addition
+	var firstName string
+	var age int
+
+	// input
+	fmt.Println("This program gets a user's name and age.")
+	fmt.Println()
+	fmt.Print("Enter your first name: ")
+	fmt.Scanln(&firstName)
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+
+	// output
+	fmt.Println("Your info is:", firstName, "age", age, ".")
+  
+	fmt.Println("\nDone.")
+}
